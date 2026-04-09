67efa6f46af01ba870e855de29ddc278ef69b8ec
Pablo Jara <41595956+jaranine@users.noreply.github.com>
2026-04-09 13:38:03 +0200

    feat: configurar getter/setter y constructor de las clases empleado y restaurante

--- src/main/java/com/restaurantes/model/Empleado.java
+++ src/main/java/com/restaurantes/model/Empleado.java
@@ -9,9 +9,71 @@ public class Empleado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-
     private String nombre;
     private String apellido;
     private String dni;
     private Integer edad;
+
+    public Empleado(Integer edad, String dni, String apellido, String nombre, Long id) {
+        this.edad = edad;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public Empleado() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
 }
--- src/main/java/com/restaurantes/model/Restaurante.java
+++ src/main/java/com/restaurantes/model/Restaurante.java
@@ -29,9 +29,73 @@ public class Restaurante {
     private Double precioMedio;
 
     @Column(columnDefinition = "BOOLEAN DEFAULT true") // Valor por defecto
-    private Boolean active;
+    private Boolean active = true;
 
     private Integer numeroEmpleados;
+
+    // metodo constructor para crear Restaurantes con valores
+    public Restaurante(Double precioMedio, String nombre, Integer numeroEmpleados) {
+        this.precioMedio = precioMedio;
+        this.nombre = nombre;
+        this.numeroEmpleados = numeroEmpleados;
+    }
+
+    // metodo constructor vacio
+    public Restaurante(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecioMedio() {
+        return precioMedio;
+    }
+
+    public void setPrecioMedio(Double precioMedio) {
+        this.precioMedio = precioMedio;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Integer getNumeroEmpleados() {
+        return numeroEmpleados;
+    }
+
+    public void setNumeroEmpleados(Integer numeroEmpleados) {
+        this.numeroEmpleados = numeroEmpleados;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "Restaurante{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precioMedio=" + precioMedio +
+                ", active=" + active +
+                ", numeroEmpleados=" + numeroEmpleados +
+                '}';
+    }
+
 
     // Próximas tareas:
     // Enum
