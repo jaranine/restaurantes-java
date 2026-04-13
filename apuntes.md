928112edbee7726b963e3fc9c3c69fc66c14fcf9
Pablo Jara <41595956+jaranine@users.noreply.github.com>
2026-04-13 09:06:58 +0200

    feat: añadir carpeta de apuntes

---
+++ apuntes.md
@@ -0,0 +1,16 @@
+# Próximas tareas:
+    Enum
+    Fecha
+    Asociación con otra entidad/tabla
+
+# SQL
+
+    SELECT * FROM RESTAURANTE
+    
+    INSERT INTO restaurante (id, nombre, numero_empleados) VALUES (3, 'Dominos', 4)
+    
+    UPDATE restaurante
+    SET numero_empleados = 15
+    WHERE id = 1;
+    
+    DELETE FROM restaurante WHERE id = 1;
--- src/main/java/com/restaurantes/model/Restaurante.java
+++ src/main/java/com/restaurantes/model/Restaurante.java
@@ -2,20 +2,6 @@ package com.restaurantes.model;
 
 import jakarta.persistence.*;
 
-/*
-
-SELECT * FROM RESTAURANTE
-
-INSERT INTO restaurante (id, nombre, numero_empleados) VALUES (3, 'Dominos', 4)
-
-UPDATE restaurante
-SET numero_empleados = 15
-WHERE id = 1;
-
-DELETE FROM restaurante WHERE id = 1;
-
-*/
-
 @Entity
 @Table(name = "Restaurantes")
 public class Restaurante {
@@ -95,10 +81,4 @@ public class Restaurante {
                 ", numeroEmpleados=" + numeroEmpleados +
                 '}';
     }
-
-
-    // Próximas tareas:
-    // Enum
-    // Fecha
-    // Asociación con otra entidad/tabla
 }
